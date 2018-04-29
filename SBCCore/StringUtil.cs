98893c6fd114876488f555d6a7ffab3a607fe267
FIVIL <31272779+FIVIL@users.noreply.github.com>
2018-04-29 07:31:15 +0430

    Wallet and Signture done.

--- SBCCore/StringUtil.cs
+++ SBCCore/StringUtil.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using Blake2Sharp;
 using Org.BouncyCastle.Asn1.Ocsp;
@@ -9,7 +10,7 @@ using Org.BouncyCastle.Security;
 
 namespace NoobChain
 {
-    public static class StringUtil
+    static class StringUtil
     {
         public static string ApplyBlacke2(this string data)
         {
@@ -31,7 +32,23 @@ namespace NoobChain
             var dsa = SignerUtilities.GetSigner("ECDSA");
             dsa.Init(true, PrivateKey);
             dsa.BlockUpdate(bytetext, 0, bytetext.Length);
-            return dsa.GenerateSignature();         
+            return dsa.GenerateSignature();
+        }
+        public static string ToStringKey(this AsymmetricKeyParameter Key)
+        {
+            TextWriter textWriter = new StringWriter();
+            var pemWriter = new Org.BouncyCastle.OpenSsl.PemWriter(textWriter);
+            pemWriter.WriteObject(Key);
+            pemWriter.Writer.Flush();
+            return textWriter.ToString().Replace("BEGIN PUBLIC KEY", "")
+                .Replace("BEGIN EC PRIVATE KEY", "")
+                .Replace("END EC PRIVATE KEY", "")
+                .Replace("-", "")
+                .Replace("END PUBLIC KEY", "")
+                .Replace(" ", "")
+                .Replace("\r\n", "")
+                .Replace("\r", "")
+                .Replace("\n", "");
         }
     }
 }
--- SBCCore/Transaction.cs
+++ SBCCore/Transaction.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Org.BouncyCastle.Crypto.Parameters;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,14 +8,14 @@ namespace NoobChain
     public class Transaction
     {
         public string ID { get; private set; }
-        public string Sender { get; private set; }
-        public string Reciepient { get; private set; }
+        public ECPublicKeyParameters Sender { get; private set; }
+        public ECPublicKeyParameters Reciepient { get; private set; }
         public double Value { get; private set; }
         public byte[] Signature { get; private set; }
         public List<TransactionInput> Inputs { get; private set; }
         public List<TransactionOutput> Outputs { get; private set; }
         private static UInt64 sequence = 0;
-        public Transaction(string from,string to,double value,List<TransactionInput> inputs)
+        public Transaction(ECPublicKeyParameters from, ECPublicKeyParameters to,double value,List<TransactionInput> inputs)
         {
             Sender = from;
             Reciepient = to;
@@ -24,7 +25,17 @@ namespace NoobChain
         private string GetHashString()
         {
             sequence++;
-            return (Sender + Reciepient + Value.ToString() + sequence.ToString()).ApplyBlacke2();
+            return (Sender.ToStringKey() + Reciepient.ToStringKey() + Value.ToString() + sequence.ToString()).ApplyBlacke2();
+        }
+        public void GenerateSignature(ECPrivateKeyParameters privateKey)
+        {
+            string data = Sender.ToStringKey() + Reciepient.ToStringKey() + Value.ToString();
+            Signature = data.GenerateSignture(privateKey);
+        }
+        public bool VerifiySignature()
+        {
+            string data = Sender.ToStringKey() + Reciepient.ToStringKey() + Value.ToString();
+            return data.VerifySignature(Sender, Signature);
         }
         public class TransactionInput
         {
