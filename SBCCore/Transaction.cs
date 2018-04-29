b0afdf4f6bb02732ccb701ed12c57e56bd7c3501
FIVIL <31272779+FIVIL@users.noreply.github.com>
2018-04-29 21:54:11 +0430

    TransactionInput/Output done.

--- SBCCore/Transaction.cs
+++ SBCCore/Transaction.cs
@@ -15,7 +15,7 @@ namespace NoobChain
         public List<TransactionInput> Inputs { get; private set; }
         public List<TransactionOutput> Outputs { get; private set; }
         private static UInt64 sequence = 0;
-        public Transaction(ECPublicKeyParameters from, ECPublicKeyParameters to,double value,List<TransactionInput> inputs)
+        public Transaction(ECPublicKeyParameters from, ECPublicKeyParameters to, double value, List<TransactionInput> inputs)
         {
             Sender = from;
             Reciepient = to;
@@ -32,18 +32,40 @@ namespace NoobChain
             string data = Sender.ToStringKey() + Reciepient.ToStringKey() + Value.ToString();
             Signature = data.GenerateSignture(privateKey);
         }
-        public bool VerifiySignature()
+        public bool IsSignatureVerified
         {
-            string data = Sender.ToStringKey() + Reciepient.ToStringKey() + Value.ToString();
-            return data.VerifySignature(Sender, Signature);
+            get
+            {
+                string data = Sender.ToStringKey() + Reciepient.ToStringKey() + Value.ToString();
+                return data.VerifySignature(Sender, Signature);
+            }
         }
         public class TransactionInput
         {
-
+            public string TransactionOutputId { get; private set; }
+            public TransactionOutput UTXO { get; private set; }
+            public TransactionInput(string TOID)
+            {
+                TransactionOutputId = TOID;
+            }
         }
         public class TransactionOutput
         {
-
+            public string ID { get; private set; }
+            public ECPublicKeyParameters Reciepient { get; private set; }
+            public double Value { get; private set; }
+            public string ParentTransactionId { get; private set; }
+            public TransactionOutput(ECPublicKeyParameters reciepient, double value,string parentTransactionId)
+            {
+                Reciepient = reciepient;
+                Value = value;
+                ParentTransactionId = parentTransactionId;
+                ID = (Reciepient.ToStringKey() + Value.ToString() + parentTransactionId).ApplyBlacke2();
+            }
+            public bool IsMine(ECPublicKeyParameters Key)
+            {
+                return (Key == Reciepient);
+            }
         }
     }
 }
--- SimpleBlockChain/Program.cs
+++ SimpleBlockChain/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using NoobChain;
 namespace SimpleBlockChain
 {
